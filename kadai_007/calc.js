115c773172aa7146e804f31be5edbb4e1cfb436c
tako2024 <iyoda551@gmail.com>
2025-03-08 11:48:25 +0900

    課題提出　kadai_007

---
+++ kadai_007/calc.js
@@ -0,0 +1,17 @@
+//変数を代入する
+let num = 30;
+
+if (num !== 0 && num%3 == 0 && num%5 == 0) {
+  console.log('3と5の倍数です');
+}
+else if (num !== 0 && num%3 == 0) {
+  console.log('3の倍数です');
+}
+else if (num !== 0 && num%5 == 0) {
+  console.log('5の倍数です');
+}
+else {
+  console.log(num);
+}
+
+
